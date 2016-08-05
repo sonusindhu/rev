321d825de9a97615c5f1fd50ee622d76d28ab9a3
Sonu Sindhu <sonusindhu10@yahoo.com>
2016-08-05 11:31:03 +0530

    layout modified

--- resources/views/layouts/cfooter.blade.php
+++ resources/views/layouts/cfooter.blade.php
@@ -1,92 +1,51 @@
-<!--footer-->
-<section class="fotr">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="col-xs-6 col-sm-3 col-md-3 ">
-                    <div class="footr_link ">
-                        <h4>Vouchers</h4>
-                        <ul>
-                            <li><a href="/deals">Deal Vouchers</a> </li>
-                            <li><a href="/gift">Gift Vouchers</a></li>
-                            <li><a href="/discount">Discount Vouchers</a></li>
-                            <li><a href="/offers">Offer Vouchers</a></li>
-                            <li><a href="/groups">Group Vouchers</a></li>
-                        </ul>
+<script src="{!! asset('/js/angular.min.js') !!}"></script>
+        <script src="{!! asset('/js/ui-bootstrap-tpls.min.js') !!}"></script>
+        <script src="{!! asset('/js/angular-file-upload.js') !!}"></script>
+        <script src="{!! asset('/js/angular-sanitize.min.js') !!}"></script>
+        <script src="{!! asset('/js/ckeditor/ckeditor.js') !!}"></script>
+        
+        
+        <!-- include our JavaScript -->
+        <script src="{!! asset('/js/app.js') !!}"></script>
+        <script src="{!! asset('/js/role.js') !!}"></script>
+        <script src="{!! asset('/js/employee.js') !!}"></script>
+        <script src="{!! asset('/js/company_custom.js') !!}"></script>
 
-                    </div>
+        
+        <style>
+            .error-message {
+                color: #ff0f14;
+                font-size: 14px;
+                margin-top: -13px;
+                position: absolute;
+                top: 50px;
+            }
+            .error_border{
+                border: 1px solid red !important;
+            }
+            input.ng-invalid-pattern{
+                border: 1px solid red !important;
+            }
+            .portfolioloader {
+                background: rgba(255, 255, 255, 0.5) none repeat scroll 0 0;
+                border-radius: 50%;
+                bottom: 48%;
+                height: 94px;
+                left: 0;
+                margin: 0 auto;
+                position: fixed;
+                right: 0;
+                top: 48%;
+                width: 100px;
+                z-index: 1000001;
+            }
 
-                </div>
-                <div class="col-xs-6 col-sm-3 col-md-3">
-                    <div class="footr_link ">
-                        <h4>Company</h4>
-                        <ul>
-                            <li><a href="/about">About Club Vouchers</a> </li>
-                            <li><a href="/careers">Careers</a></li>
-                            <li><a href="/contact">Contact us</a></li>
+        </style>
+        <div class="portfolioloader" style="display: none;"><img src="{!! asset('/img/loaderimage.gif') !!}"></div>
+        
+ <input id="logoutSetting" type="hidden" value="<?php echo @$settingsAre['SuperadminSetting']['logout_time']; ?>"  not_auto_logout_users="<?php echo @$settingsAre['SuperadminSetting']['not_auto_logout_users'] ?>"/>
+        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script src="https://code.highcharts.com/modules/exporting.js"></script>
 
-                        </ul>
-
-                    </div>
-
-                </div>
-                <div class="col-xs-6 col-sm-3 col-md-3 ">
-                    <div class="footr_link ">
-                        <h4>Legal</h4>
-                        <ul>
-                            <li><a href="/terms">Terms of use</a> </li>
-                            <li><a href="/privacy">Privacy Policy</a></li>
-                            <li><a href="/cookies">Cookies</a></li>
-
-                        </ul>
-
-                    </div>
-
-                </div>
-
-                <div class="col-xs-6 col-sm-3 col-md-3 ">
-
-                    <div class="social_media">
-                        <ul>
-                            <li class="folow_us">Follow us:</li>
-                            <li><a href="#"><img src="images/fb.png" alt="fb"/></a></li>
-                            <li><a href="#"><img src="images/twitter.png" alt="twitter"/></a></li>
-                            <li><a href="#"><img src="images/google-plus.png" alt="googleplus"/></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="col-md-12">
-                <div class="col-md-12">
-                    <div class="footr_middle_sec">
-                        <div class="col-sm-4 col-md-3 ftr_logo spc">
-                            <a href="/"><img src="images/fotr_logo.jpg" alt="logo"/></a>
-                        </div>
-                        <div class="col-sm-8 col-md-9 spc pull-right">
-                            <p>Club Vouchers For Business is a great way to introduce new customers to your 
-                                business and reward existing customers with “Thank you” discounts and offers. 
-                                It is part of a “Customer Loyalty Marketing Programme” that works to bring your 
-                                customers back in again and again at a very low cost to you.<a href="/findmore"> Find out more.</a></p>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="col-md-12 footr_last_link">
-                    <small>© 2015 Club Vouchers Ltd. Registered in England & Wales, All rights reserved</small>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-    </div>
-
-</section>
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-</body>
+    </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,55 +113,8 @@
 
                     });
         </script>
-
-        <script src="{!! asset('/js/angular.min.js') !!}"></script>
-        <script src="{!! asset('/js/ui-bootstrap-tpls.min.js') !!}"></script>
-        <script src="{!! asset('/js/angular-file-upload.js') !!}"></script>
-        <script src="{!! asset('/js/angular-sanitize.min.js') !!}"></script>
-        <script src="{!! asset('/js/ckeditor/ckeditor.js') !!}"></script>
         
         
-        <!-- include our JavaScript -->
-        <script src="{!! asset('/js/app.js') !!}"></script>
-        <script src="{!! asset('/js/role.js') !!}"></script>
-        <script src="{!! asset('/js/employee.js') !!}"></script>
-        <script src="{!! asset('/js/company_custom.js') !!}"></script>
+        @include('layouts/cfooter'); <!-- app/views/angular-stuff.php -->
 
         
-        <style>
-            .error-message {
-                color: #ff0f14;
-                font-size: 14px;
-                margin-top: -13px;
-                position: absolute;
-                top: 50px;
-            }
-            .error_border{
-                border: 1px solid red !important;
-            }
-            input.ng-invalid-pattern{
-                border: 1px solid red !important;
-            }
-            .portfolioloader {
-                background: rgba(255, 255, 255, 0.5) none repeat scroll 0 0;
-                border-radius: 50%;
-                bottom: 48%;
-                height: 94px;
-                left: 0;
-                margin: 0 auto;
-                position: fixed;
-                right: 0;
-                top: 48%;
-                width: 100px;
-                z-index: 1000001;
-            }
-
-        </style>
-        <div class="portfolioloader" style="display: none;"><img src="/img/loaderimage.gif"></div>
-        
- <input id="logoutSetting" type="hidden" value="<?php echo @$settingsAre['SuperadminSetting']['logout_time']; ?>"  not_auto_logout_users="<?php echo @$settingsAre['SuperadminSetting']['not_auto_logout_users'] ?>"/>
-        <script src="https://code.highcharts.com/highcharts.js"></script>
-        <script src="https://code.highcharts.com/modules/exporting.js"></script>
-
-    </body>
-</html>
